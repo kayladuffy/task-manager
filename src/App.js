3dfb61b61e8534d613ee6e26cbd814ea5f26d96c
Kayla.duffy <kayla.duffy@whag18kayla.lan>
2020-09-03 20:22:01 -0500

    Minor styling to todo and contact form

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
-import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import React, { Component } from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Navbar from '../src/Navbar/Navbar';
+import Form from './ContactForm/Form';
+import Home from './Home'
 
-
-
-function App() {
-  return (
-    <BrowserRouter>
-      <div className="App">
-       <Navbar/>
-      </div>
-    </BrowserRouter>
-  );
+class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <div className="App">
+          <Navbar />
+          <Switch>
+            <Route exact path='/' component={Home} />
+            <Route path='/form' component={Form} />
+          </Switch>
+        </div>
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;
