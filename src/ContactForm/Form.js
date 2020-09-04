4f2b553b9123772d40419cfc83c22398e04c2a40
Kayla.duffy <kayla.duffy@whag18kayla.lan>
2020-09-03 20:21:28 -0500

    Started contact form

---
+++ src/ContactForm/Form.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+class Form extends Component {
+    state = {
+        title: '',
+        content: ''
+    }
+    render() {
+        return (
+            <div id="fillout" className="container">
+                <form onSubmit={this.handleSubmit} className="white">
+                    <h5 className="grey-text text-darken-3">Contact Us</h5>
+                    <div className="input-field">
+                        <label htmlFor="title">First Name</label>
+                        <input type="text" id="title" onChange={this.handleChange} />
+                    </div>
+                    <div className="input-field">
+                        <label htmlFor="content">Last Name</label>
+                        <textarea id="content" className="materialize-textarea" onChange={this.handleChange}></textarea>
+                    </div>
+                    <div className="input-field">
+                        <label htmlFor="content">Email</label>
+                        <textarea id="content" className="materialize-textarea" onChange={this.handleChange}></textarea>
+                    </div>
+                    <div className="input-field">
+                        <label htmlFor="content">Message</label>
+                        <textarea id="content" className="materialize-textarea" onChange={this.handleChange}></textarea>
+                    </div>
+                    <div className="input-field">
+                        <button className="btn pick lighten-1 z-depth-0">Submit</button>
+                    </div>
+                </form>
+
+            </div>
+        )
+    }
+}
+
+
+export default Form
